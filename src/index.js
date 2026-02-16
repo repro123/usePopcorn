5bd212249d3f54a7a13838229fbb39d1c09f18bc
Ibrahim Bello <ibrahimopeyemibello@gmail.com>
2026-02-16 14:56:25 +0100

    created star component and made it reuseable

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="blue" className="test" defaultRating={2} />
   </React.StrictMode>,
 );
